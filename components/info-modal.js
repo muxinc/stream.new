04cddc23b738ebf4adf2e0c5dc554c8cd5cbc301
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-09-01 18:48:17 -0700

    add terms page

--- components/info-modal.js
+++ components/info-modal.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { breakpoints } from '../style-vars';
 import { MUX_HOME_PAGE_URL, OPEN_SOURCE_URL } from '../constants';
 
@@ -6,6 +7,7 @@ export default function InfoModal ({ close }) {
     <div className="container">
       <div className="wrapper">
         <h1>This is an <a href={OPEN_SOURCE_URL}>open source</a> project from <a href={MUX_HOME_PAGE_URL}>Mux</a>, the video streaming API.</h1>
+        <div className="terms"><Link href="/terms"><a onClick={close} role="link" onKeyDown={close} tabIndex={0}>View Terms</a></Link></div>
       </div>
       <footer>
         <div className="footer-link"><a role="presentation" onClick={close}>Close</a></div>
@@ -15,6 +17,11 @@ export default function InfoModal ({ close }) {
           padding: 20px;
           flex-grow: 1;
           display: flex;
+          flex-direction: column;
+        }
+        .terms {
+          margin-top: 40px;
+          font-size: 26px;
         }
         @media only screen and (min-width: ${breakpoints.md}px) {
           .wrapper {
---
+++ pages/terms.js
@@ -0,0 +1,48 @@
+import Layout from '../components/layout';
+import { MUX_HOME_PAGE_URL } from '../constants';
+import { breakpoints } from '../style-vars';
+
+export default function Terms () {
+  return (
+    <Layout>
+      <div className="wrapper">
+        <div>
+          <h1>Terms</h1>
+        </div>
+        <div>
+          <p>
+            The deal is simple: you upload a video, your friends at <a href={MUX_HOME_PAGE_URL}>Mux</a>
+            {' '}will stream it for a month, or until it costs us more than our CEO says is ok.
+            After that, it will be deleted.
+          </p>
+          <p>
+            As developers, we wanted a fast, frictionless way to share screencasts, desktop recordings,
+            meetup talks, etc. that didn't include much overhead and where playback would Just Work for
+            anyone with a link. So...that's what we built this for, but other stuff is ok too as long as
+            it's nice and clean. We reserve the right to take down *any* piece of content, but anything
+            hateful, pornographic, gore-y, or illegal is going to get insta-deleted.
+          </p>
+        </div>
+      </div>
+      <style jsx>{`
+        p {
+          font-size: 18px;
+          line-height: 20px;
+        }
+        @media only screen and (min-width: ${breakpoints.md}px) {
+          .wrapper {
+            flex-grow: 1;
+            flex-direction: column;
+            display: flex;
+            justify-content: center;
+          }
+          p {
+            font-size: 26px;
+            line-height: 28px;
+          }
+        }
+      `}
+      </style>
+    </Layout>
+  );
+}
