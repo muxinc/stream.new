ddd9590e197f12b925b346422ac3ef4347d7bd77
Ray KananI <hello@raykanani.com>
2021-01-30 21:56:16 -0500

    update host url

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 export const MUX_HOME_PAGE_URL = 'https://mux.com?utm_source=stream-new';
 export const MUX_TERMS_URL = 'https://mux.com/terms';
 export const OPEN_SOURCE_URL = 'https://github.com/muxinc/stream.new';
-export const HOST_URL = 'https://stream.new';
+export const HOST_URL = 'https://stream-new-beta.vercel.app';
