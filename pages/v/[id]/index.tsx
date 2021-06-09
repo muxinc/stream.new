a91b095f497967113a4ccc0506192c7f9bab2ee4
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2021-06-09 20:39:41 +0100

    Cleanup with Ed

--- pages/v/[id]/index.tsx
+++ pages/v/[id]/index.tsx
@@ -9,7 +9,7 @@ import Layout from '../../../components/layout';
 import ReportForm from '../../../components/report-form';
 import { HOST_URL } from '../../../constants';
 import logger from '../../../lib/logger';
-import urlutils from '../../../lib/urlutils'
+import { getImageBaseUrl } from '../../../lib/urlutils'
 
 type Params = {
   id: string;
@@ -24,7 +24,7 @@ export type Props = {
 export const getStaticProps: GetStaticProps = async (context)  => {
   const { params } = context;
   const { id: playbackId } = (params as Params);
-  const poster = `${urlutils.getImageBaseUrl()}/${playbackId}/thumbnail.png`;
+  const poster = `${getImageBaseUrl()}/${playbackId}/thumbnail.png`;
   const shareUrl = `${HOST_URL}/v/${playbackId}`;
 
   return { props: { playbackId, shareUrl, poster } };
