c71d78aac80dca9eb237b19c90f02a1922225f41
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2021-06-09 21:44:10 +0100

    Clean version of moderation

---
+++ lib/urlutils.ts
@@ -0,0 +1,9 @@
+const deliveryDomain = process.env.NEXT_PUBLIC_MUX_BYO_DOMAIN || "mux.com"
+
+export function getStreamBaseUrl() {
+  return `https://stream.${deliveryDomain}`;
+}
+
+export function getImageBaseUrl() {
+  return `https://image.${deliveryDomain}`;
+}
