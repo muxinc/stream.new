9730ed3ec385e9d2e47548115e95d22410512d10
Phil Cluff <578330+GeneticGenesis@users.noreply.github.com>
2021-06-09 16:30:54 +0100

    More thumbails

--- lib/moderation-utils.ts
+++ lib/moderation-utils.ts
@@ -1,9 +1,10 @@
 
 export function getThumbnailUrls ({ playbackId, duration }: { playbackId: string, duration: number }): string[] {
   /*
-   * Get 3 thumbnails based on the duration
+   * Get 5 thumbnails, weighted towards the middle of the content, based on the duration
+   * TODO: Make this more dependent on the duration - this is wasteful for short video.
    */
-  const timestamps = [(duration * 0.25),  (duration * 0.5), (duration * 0.75)];
+  const timestamps = [(duration * 0.25), (duration * 0.33),  (duration * 0.5), (duration * 0.66), (duration * 0.75)];
   const urls = timestamps.map((time) => `https://image.mux.com/${playbackId}/thumbnail.png?time=${time}`);
   return urls;
 }
