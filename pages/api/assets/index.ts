59dc75e475f680e3ced676cf9425ad43fc46f730
Amin Amos <aminamos@users.noreply.github.com>
2022-04-01 11:07:09 -0700

    some progress

---
+++ pages/api/assets/index.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Mux from '@mux/mux-node';
+
+const { Video } = new Mux();
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const { method, body } = req;
+
+  switch (method) {
+    case 'POST':
+      try {
+        const asset = await Video.Assets.create({
+          input: req.body,
+          "playback_policy": [
+            "public"
+          ],
+        });
+        res.json({
+          id: asset.id,
+          status: asset.status,
+        });
+      } catch (e) {
+        res.statusCode = 500;
+        console.error('Request error', e); // eslint-disable-line no-console
+        res.json({ error: 'Error creating upload' });
+      }
+      break;
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
