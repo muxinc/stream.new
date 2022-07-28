ee2ec3021c69f08c80510b082c469a79d967e025
clearlythuydoan <clearlythuydoan@gmail.com>
2022-07-28 15:46:37 -0400

    chore(stream.new): Remove unused CSS.

--- components/drag-overlay.tsx
+++ components/drag-overlay.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MuxUploaderDrop } from '@mux/mux-uploader-react';
-import { breakpoints } from '../style-vars';
 
 type Props = {
   dragActive?: boolean | undefined,
@@ -14,42 +13,9 @@ const DragOverlay: React.FC<Props> = ({ children, dragActive }) => {
   return (
     <>
       {/* TO-DO: Allow adjusting text styles with overlay. CSS vars? Bake into package CSS styles? (TD). */}
-      <MuxUploaderDrop className="drag-overlay" overlay overlayText="Upload to stream.new" mux-uploader="uploader" style={{ position: 'static', 
-          display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <MuxUploaderDrop overlay overlayText="Upload to stream.new" mux-uploader="uploader">
         {children}
       </MuxUploaderDrop>
-      <style jsx>{`
-        .drag-overlay {
-          height: 100%;
-          width: 100%;
-          position: absolute;
-          z-index: 1;
-          background-color:  rgba(226, 253, 255, 0.95);
-          transition: 0.5s;
-          display: none;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .drag-overlay h1 {
-          font-size: 46px;
-          line-height: 46px;
-          text-align: center;
-        }
-
-        .drag-overlay.active {
-          display: flex;
-        }
-
-        @media only screen and (min-width: ${breakpoints.md}px) {
-          .drag-overlay h1 {
-            font-size: 96px;
-            line-height: 120px;
-          }
-        }
-      `}
-      </style>
     </>
   );
 };
