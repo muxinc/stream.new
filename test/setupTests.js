723929b996acf22166c98a9b4c18a962034ec886
Dylan Jhaveri <djhaveri@mux.com>
2024-01-12 10:35:27 -0800

    add node shim for fetch

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,6 +1,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import dotenv from 'dotenv';
+import '@mux/mux-node/shims/node'
 
 dotenv.config({ path: '.env.test' });
 
