124d498dc9107e1f342116f3ead8fbd82ccf71b0
Walker <wfrankenberg@mux.com>
2026-03-25 14:18:10 -0700

    remove got

--- lib/got-client.ts
+++
@@ -1,9 +0,0 @@
-import got, { Options } from 'got';
-
-const options = {} as Options;
-
-if (process.env.NODE_ENV === 'test') {
-  options.retry = { limit: 0 };
-}
-
-export default got.extend(options);
