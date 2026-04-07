554b7381bf47011c8008538a42c4a8b0f843486c
Justin Sanford <justin.h.sanford@gmail.com>
2026-04-07 13:22:51 -0700

    remove jose transform ignore

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['/node_modules/(?!jose/)'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
