06c60fce657dc4254879e57cd56526e7f9ea06ae
Phil <578330+philcluff@users.noreply.github.com>
2026-04-14 19:25:01 +0100

    Update PII config in Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -10,5 +10,5 @@ Sentry.init({
 
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
 });
