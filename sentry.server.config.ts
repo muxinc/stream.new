9ba0cdce34636195e238c30961a594946d3d4282
Phil <578330+philcluff@users.noreply.github.com>
2026-04-14 17:28:02 +0100

    Yoloing in sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://855545284ab436cbdb4b6fe01042f189@o43841.ingest.us.sentry.io/4511219257376768",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
